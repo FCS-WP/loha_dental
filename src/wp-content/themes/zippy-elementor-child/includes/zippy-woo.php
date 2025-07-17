ff9522aa0418fa28506bc1d13d6ab93d5537968e
nhanlt-FLS <nhan.letrong@floatingcube.com>
2025-07-17 14:31:13 +0700

    update-flatprickr-timepicker

--- src/wp-content/themes/zippy-elementor-child/includes/zippy-woo.php
+++
@@ -1,8 +0,0 @@
-<?php
-add_action('wp_enqueue_scripts', 'flatpickr_enqueue_scripts');
-
-function flatpickr_enqueue_scripts()
-{
-    wp_enqueue_script('flatpickr-js', get_template_directory_uri() . '/assets/lib/flatpickr/flatpickr.min.js', array('jquery'), null, true);
-    wp_enqueue_style('flatpickr-css', get_template_directory_uri() . '/assets/lib/flatpickr/flatpickr.min.css');
-}
